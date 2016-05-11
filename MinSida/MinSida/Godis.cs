bfee843cf95c14cc72191d000e0a8161c1199c27
Nerea <nerea.ribes-karabay@yh.nackademin.se>
2016-05-11 12:49:49 +0200

    Lägger till en ny klass i develop

---
+++ MinSida/MinSida/Godis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinSida
+{
+    public class Godis
+    {
+        //Jag lägger till en ny class som heter Godis i develop
+    }
+}
